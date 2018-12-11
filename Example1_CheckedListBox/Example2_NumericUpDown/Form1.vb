428e81b404576efe1c5fad4b32f810c8a5841a6d
kitsana phomgarm <nick7266@hotmail.com>
2018-12-11 15:13:09 +0700

    Ex4 Complete

--- Example1_CheckedListBox/Example2_NumericUpDown/Form1.vb
+++ Example1_CheckedListBox/Example2_NumericUpDown/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
+    Private Sub Numeric_ValueChanged(sender As Object, e As EventArgs) Handles Numeric.ValueChanged
+        Dim number As Integer = Numeric.Value
+        LabelNumeric.Text = number
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim number As Integer = Numeric.Value
+        LabelNumeric.Text = number
+    End Sub
 End Class
--- Example1_CheckedListBox/Example3_DomainUpDown/Form1.vb
+++ Example1_CheckedListBox/Example3_DomainUpDown/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub DayUpDown_SelectedItemChanged(sender As Object, e As EventArgs) Handles DayUpDown.SelectedItemChanged
+        Dim item As String = DayUpDown.SelectedItem
+        Dim day() As String = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                               "Friday", "Saturday", "Sunday"}
+        Dim count As Integer = DayUpDown.Items.Count - 1
 
+        For i = 0 To count
+            If DayUpDown.SelectedIndex = i Then
+                LabelDay.Text = day(i)
+            End If
+        Next
+    End Sub
 End Class
